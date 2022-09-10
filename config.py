e8a565c1f9aa46a2cb4af88a7da66a8e6340f749
daye10 <72979772+daye10@users.noreply.github.com>
2022-09-09 21:44:00 -0400

    config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+'''
+Note: Since ver.5.0.0 and safetynet implementation in the JP Version, there is no way to create a JP account from the bot !!
+So the bot only works with GLB version, even if options for the JP version are displayed, it will return to the GLB version.
+
+This is a pre-release version, the bot will work but is not optinal, there are still several things to add/modify/delete,
+if you have a comment, do not hesitate to let me know! 
+
+(See README.md for more info)
+
+'''
+
+client = 'global'
+global_version = '5.5.2'
+bot_app_version = 'alpha-1.0.2'
+
+release_url = 'https://github.com/daye10/dokkan-battle-farm-bot-app/releases' 
+disscussion_url = 'https://github.com/daye10/dokkan-battle-farm-bot-app/discussions'
+report_issue_url = 'https://github.com/daye10/dokkan-battle-farm-bot-app/issues'
+
+
